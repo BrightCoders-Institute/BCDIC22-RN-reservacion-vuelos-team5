2910358bd57931d7c3e1d188eb5faa202748962f
IvanGodinez21 <ivano.godinez@gmail.com>
2023-02-09 21:29:49 -0600

    feat: :wrench: Flights are now displayed in MyFlights' screen
    Modified:
    - MyFlightsCard now use props
    - BookingPicker updates formikprops values on mount

--- src/interfaces/MyFlights.ts
+++ src/interfaces/MyFlights.ts
@@ -1,6 +1,11 @@
 // Node modules
+import { FirebaseFirestoreTypes } from '@react-native-firebase/firestore';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 // Interfaces
 import { RootStackParamList } from './RootStackParamList';
 
 export type IMyFlightsProps = NativeStackScreenProps<RootStackParamList, 'MyFlights'>;
+
+export interface IMyFlightsState {
+  flights: FirebaseFirestoreTypes.DocumentData[];
+}
--- src/screens/MyFlights.tsx
+++ src/screens/MyFlights.tsx
@@ -1,28 +1,53 @@
 // Node modules
 import React, { Component } from 'react';
-import { ScrollView, View, Text } from 'react-native';
-
+import { FlatList, View, Text } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 // Components
 import MyFlightsCard from '../components/MyFlightsCard';
 import MyFlightsButtonAdd from '../components/MyFlightsButtonAdd';
-
+// Firebase
+import { auth } from '../firebase/firebaseConfig';
 // Interfaces
-import { IMyFlightsProps } from '../interfaces/MyFlights';
+import { IMyFlightsProps, IMyFlightsState } from '../interfaces/MyFlights';
 // Styles
 import Styles from '../styles/MyFlights';
 
-export default class LogIn extends Component<IMyFlightsProps> {
+export default class LogIn extends Component<IMyFlightsProps, IMyFlightsState> {
   constructor(props: IMyFlightsProps) {
     super(props);
+    this.state = {
+      flights: [],
+    };
   }
+
+  async componentDidMount() {
+    await this.getUserFlights();
+  }
+
+  getUserFlights = async () => {
+    const subscribe = firestore()
+      .collection('users')
+      .doc(auth().currentUser?.uid)
+      .collection('flights')
+      .onSnapshot(async (querySnapshot) => {
+        const flights = [];
+        for (const doc of querySnapshot.docs) {
+          flights.push(doc.data());
+        }
+        this.setState({ flights: flights });
+      });
+    return () => subscribe();
+  };
 
   render() {
     return (
       <View style={Styles.screen.container}>
         <Text style={Styles.screen.text}>My Flights</Text>
-        <ScrollView style={Styles.screen.scroll}>
-          <MyFlightsCard />
-        </ScrollView>
+        <FlatList
+          style={Styles.screen.scroll}
+          data={this.state.flights}
+          renderItem={({ item }) => <MyFlightsCard data={item} />}
+        />
         <MyFlightsButtonAdd
           onPress={() => {
             this.props.navigation.navigate('Booking');
